2aa845f9afbd950acb041cf98370191a4016c89b
maohong.wang <maohong.wang@horizon.ai>
2020-03-01 21:16:37 +0800

    add go mod

--- ch1/func/main.go
+++ ch1/func/main.go
@@ -1,1 +1,25 @@
-package _func
+package main
+
+import "fmt"
+
+type user struct {
+	name string
+	age byte
+}
+
+func (u user) ToString() string {
+	return fmt.Sprintf("%+v", u)
+}
+
+type manager struct {
+	user
+	title string
+}
+
+func main() {
+	var m manager
+	m.name="tom"
+	m.age = 30
+	m.title= "CTO"
+	println(m.ToString())
+}
