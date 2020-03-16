72692d23b9fc8ccaf4a6695c30b3285ac892b3ad
maohong.wang <maohong.wang@horizon.ai>
2020-03-16 21:31:02 +0800

    channel+

--- ch8-concurrency/chan/main_test.go
+++ ch8-concurrency/chan/main_test.go
@@ -1,6 +1,11 @@
 package _chan
 
-import "testing"
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+)
 
 func TestChan(t *testing.T) {
 	done := make(chan struct{})
@@ -22,5 +27,75 @@ func TestChan(t *testing.T) {
 	c <- 3
 	close(c)
 	//c <- 4  //panic
-	<-done  //阻塞，直到close(done)
+	fmt.Printf("after close chan %d\n",<-c) //从已关闭接收数据，返回已缓冲数据或零值
+	<-done  //
+}
+
+
+func TestChan2(t *testing.T) {
+	done := make(chan struct{})
+	c := make(chan int)
+
+	go func() {
+		defer close(done)
+		  for v:=range c { //chan 支持range
+		  	println(v)
+		  }
+	}()
+	c <- 1
+	c <- 2
+	c <- 3
+	close(c)
+	//c <- 4  //panic
+	<-done  //
+}
+
+//广播通知
+func TestChan3(t *testing.T) {
+	//var wg sync.WaitGroup
+	ready:=make(chan struct{})
+
+	for i:=0;i<3;i++{
+		//wg.Add(1)
+
+		go func(id int) {
+			//defer wg.Done()
+			println(id,": ready")
+			<-ready
+			println(id,": running...")
+		}(i)
+	}
+
+	time.Sleep(time.Second)
+	println("Ready? Go!")
+	close(ready)
+	//wg.Wait()
+}
+
+//单向通道
+func TestChan4(t *testing.T) {
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	c:=make(chan int)
+	var send chan<- int = c
+	var recv <-chan int =c
+
+	go func() {
+		defer wg.Done()
+		//close(recv)  //close不能用于接收端
+		for x:=range recv {
+			println(x)
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		defer close(send)
+		for i:=0;i<3;i++ {
+			send<-i
+		}
+	}()
+
+	wg.Wait()
 }
--- ch8-concurrency/main_test.go
+++ ch8-concurrency/main_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"strings"
 	"sync"
 	"testing"
 	"time"
@@ -34,4 +36,53 @@ func TestConc2(t *testing.T) {
 	println("main..")
 	wg.Wait()
 	println("main exit.")
+}
+
+
+func compressString(S string) string {
+	var newStr strings.Builder
+	length :=len(S)
+	for i:=0;i<length; {
+		count:=1
+		if length == 1 {
+			return S
+		}
+		if i==length-1 &&S[length-1] != S[length-2] {
+			newStr.WriteString(string(S[i]) + fmt.Sprintf("%d",count))
+			break
+		}
+		v :=S[i]
+		j :=i+1
+		for ;j<length;j++ {
+			if S[j]==v {
+				count++
+				if j == length-1 {
+					newStr.WriteString(string(v) + fmt.Sprintf("%d",count))
+					break;
+				}
+				continue
+			}else {
+
+				newStr.WriteString(string(v) + fmt.Sprintf("%d",count))
+				break;
+			}
+
+		}
+		i=j
+	}
+
+
+
+	s2:=newStr.String()
+
+	if len(s2)>=length {
+		return S
+	}else {
+		return s2
+	}
+
+}
+
+func Test11(t *testing.T) {
+	println(compressString("a"))
 }
